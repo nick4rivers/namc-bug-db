169e6440fdf21976b5c7cbb23ec27387e5f95713
Philip Bailey <philip@northarrowresearch.com>
2020-10-07 16:20:29 -0700

    taxa levels

---
+++ docker/postgres/initdb/13_taxa_levels.sql
@@ -0,0 +1,8 @@
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (1, 'Domain');
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (2, 'Kingdom');
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (3, 'Phylum');
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (4, 'Class');
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (5, 'Order');
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (6, 'Family');
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (7, 'Genus');
+INSERT INTO taxa_levels (taxa_level_id, taxa_level_name) VALUES (8, 'Species');
