7ff7fae3c3174abd94c8be7b9a962a55cbbec120
Philip Bailey <philip@northarrowresearch.com>
2020-10-19 11:22:32 -0700

    schema improvements, including bug_data

---
+++ docker/postgres/initdb/16_box_statuses.sql
@@ -0,0 +1,3 @@
+INSERT INTO box_statuses (status_id, box_status_name) VALUES (1, 'Waiting');
+INSERT INTO box_statuses (status_id, box_status_name) VALUES (2, 'Active');
+INSERT INTO box_statuses (status_id, box_status_name) VALUES (3, 'Completed');
