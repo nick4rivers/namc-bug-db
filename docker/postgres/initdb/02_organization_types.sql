8037aced4591e33fb1e8887b7b7f1f65e5b679e0
Philip Bailey <philip@northarrowresearch.com>
2020-10-30 19:43:48 -0700

    labs importing

---
+++ docker/postgres/initdb/02_organization_types.sql
@@ -0,0 +1,8 @@
+INSERT INTO entity.organization_types (organization_type_id, organization_type_name) VALUES (1, 'Federal');
+INSERT INTO entity.organization_types (organization_type_id, organization_type_name) VALUES (2, 'Tribe');
+INSERT INTO entity.organization_types (organization_type_id, organization_type_name) VALUES (3, 'University');
+INSERT INTO entity.organization_types (organization_type_id, organization_type_name) VALUES (4, 'Other');
+INSERT INTO entity.organization_types (organization_type_id, organization_type_name) VALUES (5, 'Private/NGO');
+INSERT INTO entity.organization_types (organization_type_id, organization_type_name) VALUES (6, 'State');
+INSERT INTO entity.organization_types (organization_type_id, organization_type_name) VALUES (7, 'International');
+
--- python/database_migration.py
+++ python/database_migration.py
@@ -16,34 +16,96 @@ from migrate_projects import ProjectMigrator
 
 def database_migration(mscon, pgcon):
 
-    print('test')
-
     pgcurs = pgcon.cursor()
     lookup = {
-        'agency_categories': load_lookup(pgcurs, 'agency_categories', 'category_id', 'category_name'),
-        'agencies': load_lookup(pgcurs, 'agencies', 'agency_id', 'agency_name'),
-        'life_stages': load_lookup(pgcurs, 'life_stages', 'life_stage_id', 'life_stage_name'),
+        # 'agency_categories': load_lookup(pgcurs, 'agency_categories', 'category_id', 'category_name'),
+        'organization_types': load_lookup(pgcurs, 'entity.organization_types', 'organization_type_id', 'organization_type_name'),
+        # 'agencies': load_lookup(pgcurs, 'agencies', 'agency_id', 'agency_name'),
+        'life_stages': load_lookup(pgcurs, 'organism.life_stages', 'life_stage_id', 'life_stage_name'),
         'ecosystems': load_lookup(pgcurs, 'ecosystems', 'ecosystem_id', 'ecosystem_name'),
-        'land_uses': load_lookup(pgcurs, 'land_uses', 'land_use_id', 'land_use_name'),
-        'habitats': load_lookup(pgcurs, 'habitats', 'habitat_id', 'habitat_name'),
-        'sample_types': load_lookup(pgcurs, 'sample_types', 'sample_type_id', 'sample_type_name'),
+        'land_uses': load_lookup(pgcurs, 'sample.land_uses', 'land_use_id', 'land_use_name'),
+        'habitats': load_lookup(pgcurs, 'sample.habitats', 'habitat_id', 'habitat_name'),
+        'sample_types': load_lookup(pgcurs, 'sample.sample_types', 'sample_type_id', 'sample_type_name'),
         'systems': load_lookup(pgcurs, 'systems', 'system_id', 'system_name'),
-        'states': load_lookup(pgcurs, 'states', 'state_id', 'abbreviation'),
-        'counties': load_lookup(pgcurs, 'counties', 'county_id', 'county_name'),
-        'taxa_levels': load_lookup(pgcurs, 'taxa_levels', 'taxa_level_id', 'taxa_level_name'),
-        'models': load_lookup(pgcurs, 'models', 'model_id', 'model_name'),
-        'sample_methods': load_lookup(pgcurs, 'sample_methods', 'sample_method_id', 'sample_method_name'),
-        'box_statuses': load_lookup(pgcurs, 'box_statuses', 'box_status_id', 'box_status_name'),
-        'datums': load_lookup(pgcurs, 'datums', 'datum_id', 'datum_name'),
-        'labs': load_lookup(pgcurs, 'labs', 'lab_id', 'lab_name')
+        'states': load_lookup(pgcurs, 'geo.states', 'state_id', 'state_name'),
+        'counties': load_lookup(pgcurs, 'geo.counties', 'county_id', 'county_name'),
+        'countries': load_lookup(pgcurs, 'geo.countries', 'country_id', 'abbreviation'),
+        'taxa_levels': load_lookup(pgcurs, 'organism.levels', 'level_id', 'level_name'),
+        # 'models': load_lookup(pgcurs, 'models', 'model_id', 'model_name'),
+        'sample_methods': load_lookup(pgcurs, 'sample.sample_methods', 'sample_method_id', 'sample_method_name'),
+        'box_statuses': load_lookup(pgcurs, 'sample.box_states', 'box_state_id', 'box_state_name')
+        # 'datums': load_lookup(pgcurs, 'datums', 'datum_id', 'datum_name'),
+        # 'labs': load_lookup(pgcurs, 'labs', 'lab_id', 'lab_name')
     }
 
-    # import_customers(mscon, pgcon, lookup)
+    mscurs = mscon.cursor()
+    mscurs.execute('SELECT * FROM PilotDB.dbo.Lab')
+    for msrow in mscurs.fetchall():
+        msdata = dict(zip([t[0] for t in msrow.cursor_description], msrow))
+        # print(msdata)
 
-    project_migrator = ProjectMigrator(mscon)
-    project_migrator.migrate(mscon, pgcon, lookup)
+        entity = {
+            'address1': msdata['Address1'],
+            'address2': msdata['Address2'],
+            'city': msdata['City'],
+            'country_id': lookup['countries']['USA'],
+            'state_id': get_state(msdata['State'], lookup),
+            'zip_code': msdata['ZipCode'],
+            'phone': msdata['PhoneNo'],
+            'fax': msdata['FaxNo'],
+            # 'email': msdata['Email'],
+            'Website': msdata['Website']
+        }
 
-    # import_boxes(mscon, pgcon, lookup)
+        cols = entity.keys()
+        entity['entity_id'] = pg_insert(pgcurs, 'entity.entities', 'entity_id', entity)
+
+        lab_desc = sanitize_string_col('Lab', 'LabID', msdata, 'LabDesc')
+        lab_name = sanitize_string_col('Lab', 'LabID', msdata, 'Name')
+        new_name = lab_desc if lab_desc else lab_name
+
+        org = {
+            'organization_name': new_name,
+            'entity_id': entity['entity_id'],
+            'abbreviation': lab_name,
+            'is_lab': True,
+            'organization_type_id': lookup['organization_types']['Private/NGO']
+        }
+
+        pg_insert(pgcurs, 'entity.organizations', 'organization_id', org)
+
+    # migration_path = os.path.join(os.path.dirname(__file__), 'migration.json')
+    # with open(migration_path) as f:
+    #     tables = json.load(f)
+
+    # for table in tables:
+    #     migrate(mscon, pgcon, lookup, table)
+
+    print('Database migration complete')
+
+
+def get_state(original, lookup):
+
+    if not original:
+        return None
+
+    for state_dict in ['states', 'states_abbr']:
+        if original in lookup[state_dict]:
+            return lookup[state_dict][original]
+
+        for name, id in lookup[state_dict].items():
+            if name.lower() == original.lower():
+                return id
+
+    return None
+
+
+def pg_insert(pgcurs, table, id_field, data):
+
+    cols = list(data.keys())
+    values = [data[col] for col in cols]
+    pgcurs.execute('INSERT INTO {} ({}) VALUES ({}) RETURNING {}'.format(table, ','.join(cols), ','.join('s' * len(cols)).replace('s', '%s'), id_field), values)
+    return pgcurs.fetchone()[0]
 
 
 def load_lookup(pgcurs, table, id_col, name_col):
@@ -52,79 +114,7 @@ def load_lookup(pgcurs, table, id_col, name_col):
     return {row[1]: row[0] for row in pgcurs.fetchall()}
 
 
-def import_customers(mscon, pgcon, lookup):
-
-    arapaho_count = 0
-
-    # Customers with missing states
-    customer_states = {
-        'BLM-NV-014': 'NV',
-        'DEQ-02': 'UT',
-        'IDEQ': 'ID',
-        'ISU': 'ID',
-        'NPS-KLMN': 'OR',
-        'Private-01': 'OR',
-        'R02F10B': 'CO',
-        'USU-Armstrong': 'UT'
-
-    }
-
-    def customer_sanitize(r, field):
-        return sanitize('Customer', 'CustID', r, field)
-
-    pgcurs = pgcon.cursor()
-
-    mscurs = mscon.cursor()
-    mscurs.execute('SELECT * FROM PilotDB.dbo.Customer')
-    for row in mscurs.fetchall():
-        r = dict(zip([t[0] for t in row.cursor_description], row))
-        data = {
-            'customer_code': customer_sanitize(r, 'CustID'),
-            'category_id': lookup_id(lookup, 'agency_categories', r, 'Category'),
-            'agency_id': lookup_id(lookup, 'agencies', r, 'Agency'),
-            'customer_name': customer_sanitize(r, 'Customer'),
-            'address1': customer_sanitize(r, 'Address1'),
-            'address2': customer_sanitize(r, 'Address2'),
-            'city': customer_sanitize(r, 'City'),
-            'state_id': lookup_id(lookup, 'states', r, 'State'),
-            'zip_code': customer_sanitize(r, 'ZipCode'),
-            'contact': customer_sanitize(r, 'Contact'),
-            'phone': customer_sanitize(r, 'Phone'),
-            'fax': customer_sanitize(r, 'Fax'),
-            'last_contact': r['LastContact'],
-            'notes': customer_sanitize(r, 'Notes')
-        }
-
-        # Replace missing states with hard coded values from above
-        if data['customer_code'] in customer_states:
-            data['state_id'] = lookup['states'][customer_states[data['customer_code']]]
-
-        if data['customer_name'] == 'USFS Arapaho-Roosevelt National Forest':
-            if arapaho_count > 0:
-                data['customer_name'] += ' ' + str(arapaho_count)
-            arapaho_count += 1
-
-        # duplicates
-        if data['customer_code'] == 'Ecoanalysts - Moscow, ID':
-            continue
-
-        cols = list(data.keys())
-        values = [data[col] for col in cols]
-        pgcurs.execute('INSERT INTO customers ({}) VALUES ({})'.format(','.join(cols), ','.join('s' * len(cols)).replace('s', '%s')), values)
-
-    pgcon.commit()
-
-    lookup['customers'] = load_lookup(pgcurs, 'customers', 'customer_id', 'customer_name')
-
-    # Add duplicates here
-    lookup['customers']['Ecoanalysts - Moscow, ID'] = lookup['customers']['EcoA-ID']
-
-
-def lookup_id(lookup, lookup_key, row, row_key):
-    return lookup[lookup_key][row[row_key]] if row[row_key] in lookup[lookup_key] else None
-
-
-def sanitize(table, id_field, row, field):
+def sanitize_string_col(origin_table, id_field, row, field):
 
     original = row[field]
 
@@ -132,27 +122,92 @@ def sanitize(table, id_field, row, field):
         return None
 
     if row[field].lower() in ['<null>', 'null']:
-        log = Logger(table)
+        log = Logger(origin_table)
         log.info('NULL literal "{}" in field {} with key {}'.format(row[field], field, row[id_field]))
         return None
 
     stripped = original.strip()
     if len(stripped) != len(original):
-        log = Logger(table)
+        log = Logger(origin_table)
         log.info('Stripped white space from "{}" in field {} with key {}'.format(row[field], field, row[id_field]))
         original = stripped
 
     if len(original) < 1:
-        log = Logger(table)
+        log = Logger(origin_table)
         log.info('Empty string converted to NULL in field {} with key {}'.format(field, row[id_field]))
         return None
 
     return original
 
 
-def import_boxes(mscon, pgcon, lookup):
+# def migrate(mscon, pgcon, lookup, table_info):
 
-    print('here')
+#     origin_table = table_info['originTable']
+#     target_table = table_info['targetTable']
+#     origin_id_field = table_info['originIdField']
+#     target_id_field = table_info['targetIdField']
+#     target_name_field = table_info['targetNameField']
+
+#     log = Logger(origin_table)
+#     error_count = 0
+
+#     mscurs = mscon.cursor()
+#     mscurs.execute('SELECT Count(*) FROM PilotDB.dbo.{}'.format(origin_table))
+#     origin_rows = mscurs.fetchone()[0]
+#     log.info('{} values in origin table {}.'.format(origin_rows, origin_table))
+
+#     pgcurs = pgcon.cursor()
+#     mscurs.execute('SELECT * FROM PilotDB.dbo.{}'.format(origin_table))
+#     for row in mscurs.fetchall():
+#         row = dict(zip([t[0] for t in row.cursor_description], row))
+
+#         data = {}
+#         for origin_field, target_field in table_info['columns'].items():
+#             if isinstance(target_field, dict):
+#                 if isinstance(target_field['lookup'], dict):
+#                     lookup_values = target_field['lookup']
+#                 else:
+#                     if target_field['lookup'] in lookup:
+#                         lookup_values = lookup[target_field['lookup']]
+#                     else:
+#                         if 'missing' in targetfield:
+#                             if row[origin_id_field] in missing:
+#                                 lookup_values
+
+#                 if row[origin_field] in lookup_values:
+#                     data[target_field['targetField']] = lookup_values[row[origin_field]]
+
+#             elif isinstance(row[origin_field], str):
+#                 data[target_field] = sanitize_string_col(origin_table, origin_id_field, row, origin_field)
+#             else:
+#                 data[target_field] = row[origin_field]
+
+#         cols = list(data.keys())
+#         values = [data[col] for col in cols]
+#         try:
+#             pgcurs.execute('INSERT INTO {} ({}) VALUES ({})'.format(target_table, ','.join(cols), ','.join('s' * len(cols)).replace('s', '%s')), values)
+#         except Exception as ex:
+#             log.error(str(ex))
+#             error_count += 1
+
+#     if error_count > 0:
+#         pgcon.rollback()
+#         raise 'Aborting table {} due to {} errors.'.format(origin_table, error_count)
+
+#     # pgcon.commit()
+
+#     pgcurs.execute('SELECT Count(*) FROM {}'.format(target_table))
+#     target_rows = pgcurs.fetchone()[0]
+
+#     if target_rows != origin_rows:
+#         log.warning('{} has {} rows, while origin table {} has {} rows.'.format(target_table, target_rows, origin_table, origin_rows))
+#     else:
+#         log.info('Table {} ({}) migrated to {} ({}) successfully.'.format(origin_table, origin_rows, target_table, target_rows))
+
+#     # Return the values as a dictionary so that they can be used as lookup
+#     if target_id_field and target_name_field:
+#         pgcurs.execute('SELECT {}, {} FROM {}'.format(target_id_field, target_name_field, target_table))
+#         lookup[target_table] = {row[1]: row[0] for row in pgcurs.fetchall()}
 
 
 def main():
@@ -182,15 +237,15 @@ def main():
     # https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-SQL-Server-from-Mac-OSX
     mscon = pyodbc.connect('DSN={};UID={};PWD={}'.format(args.msdb, args.msuser_name, args.mspassword))
 
-    try:
-        database_migration(mscon, pgcon)
+    # try:
+    database_migration(mscon, pgcon)
 
-        pgcon.commit()
-    except Exception as ex:
-        pgcon.rollback()
-        traceback.print_exc(file=sys.stdout)
-        log.error(ex)
-        sys.exit(1)
+    pgcon.commit()
+    # except Exception as ex:
+    #     pgcon.rollback()
+    #     traceback.print_exc(file=sys.stdout)
+    #     log.error(ex)
+    #     sys.exit(1)
 
     sys.exit(0)
 
