c434e859dc57eb78374bd14e66ee11d5cd374ee7
Philip Bailey <philip@northarrowresearch.com>
2020-12-10 12:09:27 -0800

    fixing typo in habitat lookup

--- docker/postgres/initdb/07_habitats.sql
+++ docker/postgres/initdb/07_habitats.sql
@@ -3,7 +3,7 @@ INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VAL
 INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (3, 'Littoral', 3, TRUE);
 INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (4, 'Multiple', 1, FALSE);
 INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (5, 'Other', 1, TRUE);
-INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (6, 'Palagic', 3, TRUE);
+INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (6, 'Pelagic', 3, TRUE);
 INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (7, 'Pelagic/Water Column', 3, TRUE);
 INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (8, 'Pool', 2, TRUE);
 INSERT INTO geo.habitats (habitat_id, habitat_name, ecosystem_id, is_active) VALUES (9, 'Reachwide', 2, TRUE);
