fcd4ff42aaf6246c7ee4ca4086f4438213c68f76
Philip Bailey <philip@northarrowresearch.com>
2020-12-10 19:55:03 -0800

    deleting old temp scripts

--- docker/postgres/temp/03_model_types.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO model_types (model_type_id, model_type_name) VALUES (1, 'MMI');
-INSERT INTO model_types (model_type_id, model_type_name) VALUES (2, 'ODE');
--- docker/postgres/temp/17_datums.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO datums (datum_id, datum_name) VALUES (1, 'NAD27');
-INSERT INTO datums (datum_id, datum_name) VALUES (2, 'NAD83');
-INSERT INTO datums (datum_id, datum_name) VALUES (3, 'NAD84');
