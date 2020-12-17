2af843eee8c2156b56422f3d3a742a3919e53887
Philip Bailey <philip@northarrowresearch.com>
2020-12-16 16:42:48 -0800

    organisms

--- python/migrate_organisms.py
+++ python/migrate_organisms.py
@@ -1,7 +1,7 @@
 import pyodbc
 from rscommons import Logger, ProgressBar
 from lookup_data import lookup_data, get_db_id
-from utilities import log_record_count
+from utilities import log_record_count, format_values
 
 
 def migrate(mscon, output_path):
@@ -11,12 +11,14 @@ def migrate(mscon, output_path):
 
     life_stages = lookup_data('life_stages', '04_life_stages.sql', 'life_stage_name')
 
+    cols = ['sample_id', 'taxonomy_id', 'life_stage_id', 'bug_size', 'split_count', 'big_rare_count']
+
     mscurs = mscon.cursor()
     mscurs.execute('SELECT * FROM PilotDB.dbo.BugData')
     counter = 0
     progbar = ProgressBar(total_rows, 50, "Migrating organisms")
     with open(output_path, 'w') as f:
-        f.write('INSERT INTO sample.organisms (sample_id, taxonomy_id, life_stage_id, bug_size, split_count, big_rare_count) VALUES\n')
+        f.write('INSERT INTO sample.organisms ({}) VALUES\n'.format(','.join(cols)))
 
         for msrow in mscurs.fetchall():
             msdata = dict(zip([t[0] for t in msrow.cursor_description], msrow))
@@ -25,17 +27,21 @@ def migrate(mscon, output_path):
             if not life_stage_id:
                 life_stage_id = life_stages['Unspecified']['life_stage_id']
 
-            f.write('{}({},{},{},{},{},{})\n'.format(
-                ',' if counter > 0 else '',
-                msdata['SampleID'],
-                msdata['Code'],
-                life_stage_id,
-                msdata['BugSize'],
-                msdata['SplitCount'],
-                msdata['BigRareCount']))
+            data = {
+                'sample_id': msdata['SampleID'],
+                'life_stage_id': life_stage_id,
+                'taxonomy_id': msdata['Code'],
+                'bug_size': msdata['BugSize'],
+                'split_count': msdata['SplitCount'],
+                'big_rare_count': int(msdata['BigRareCount']) if msdata['BigRareCount'] else None
+            }
 
+            f.write('{}({})\n'.format(',' if counter > 0 else '', format_values(cols, data)))
             counter += 1
             progbar.update(counter)
+
+            if counter > 1000:
+                break
 
         f.write(';\n')
 
