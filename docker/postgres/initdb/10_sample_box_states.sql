e87673abe3eb549d9a22be4f0142e89aa63cf684
Philip Bailey <philip@northarrowresearch.com>
2021-01-07 20:50:28 -0800

    abandoned box states

--- docker/postgres/initdb/10_sample_box_states.sql
+++ docker/postgres/initdb/10_sample_box_states.sql
@@ -4,4 +4,5 @@ INSERT INTO sample.box_states (box_state_id, box_state_name, box_state_order, de
 INSERT INTO sample.box_states (box_state_id, box_state_name, box_state_order, description) VALUES (4, 'On Hold', 4, NULL);
 INSERT INTO sample.box_states (box_state_id, box_state_name, box_state_order, description) VALUES (5, 'Active', 5, NULL);
 INSERT INTO sample.box_states (box_state_id, box_state_name, box_state_order, description) VALUES (6, 'Complete', 6, NULL);
+INSERT INTO sample.box_states (box_state_id, box_state_name, box_state_order, description) VALUES (7, 'Aborted', 7, NULL);
 
