7b3570eb1677d3fcf452ffb394d77b47cd82778e
Philip Bailey <philip@northarrowresearch.com>
2021-01-08 10:32:46 -0800

    data dictionary - migrations

--- python/data_dictionary.py
+++ python/data_dictionary.py
@@ -7,13 +7,18 @@ from rscommons import Logger, dotenv
 
 def data_dictionary(pgcon):
 
+    output_dir = os.path.join(os.path.dirname(__file__), '../docs/database')
+
     pgcurs = pgcon.cursor(cursor_factory=psycopg2.extras.DictCursor)
     pgcurs.execute('SELECT schema_id, schema_name FROM meta.schemas')
     schemas = {row['schema_name']: row['schema_id'] for row in pgcurs.fetchall()}
 
+    # Key = old table name, value = list of new tables where data reside
+    migrations = {}
+
     for schema_name, schema_id in schemas.items():
 
-        markdown_file = os.path.join(os.path.dirname(__file__), '../docs/database', 'schema_{}.md'.format(schema_name))
+        markdown_file = os.path.join(output_dir, 'schema_{}.md'.format(schema_name))
 
         pgcurs.execute('SELECT diagram_url, description FROM meta.schemas WHERE schema_id = %s', [schema_id])
         row = pgcurs.fetchone()
@@ -31,6 +36,16 @@ def data_dictionary(pgcon):
 
             pgcurs.execute('SELECT * FROM meta.tables WHERE schema_id = %s ORDER BY table_name', [schema_id])
             for row in pgcurs.fetchall():
+
+                # Build the table mapping information from the old databases to the new database
+                source_data = row['source_data']
+                if source_data:
+                    old_table_names = source_data.split(',')
+                    for old_table in old_table_names:
+                        if old_table not in migrations:
+                            migrations[old_table] = []
+                        migrations[old_table].append('{}.{}'.format(schema_name, row['table_name']))
+
                 f.write('\n# {}\n'.format(row['table_name']))
 
                 f.write('\n|Column|Data Type|Null|Description|\n')
@@ -51,6 +66,21 @@ def data_dictionary(pgcon):
                         data_type = 'TimeStamp'
 
                     f.write('|{}{}|{}|{}|{}|\n'.format(c['column_name'], pk, data_type, nullable, ''))
+
+    # Output the migrations information
+    markdown_file = os.path.join(output_dir, 'data_migration.md')
+    with open(markdown_file, 'w') as f:
+        f.write('---\ntitle: Data Migration\n---\n')
+        f.write("""\nThe table below describes where data has moved from in the old front and back end databases to the new database.
+        You can search for the name of an old front or back end database table and see which tables in the new system now hold the relevant data.
+        Note that some data has been split into two or more tables.\n
+        """)
+        f.write('\n|Old Table|New Table(s)|\n')
+        f.write('|---|---|\n')
+
+        for old_table, new_tables in migrations.items():
+            f.write('|{}|{}|\n'.format(old_table, ', '.join(t for t in new_tables)))
+
     print('data dictionary complete')
 
 
