89811ea75b53f5048d34f9ebb49cf8fa6925b1d5
Philip Bailey <philip@northarrowresearch.com>
2021-02-18 11:53:41 -0800

    queries for the API

--- docker/postgres/initdb/02_FUNCTIONS.sql
+++ docker/postgres/initdb/02_FUNCTIONS.sql
@@ -1,4 +1,61 @@
 /******************************************************************************************************************
+geo SCHEMA
+*/
+CREATE OR REPLACE FUNCTION geo.fn_site_info(p_site_id INT)
+    RETURNS TABLE
+            (
+                site_id             INT,
+                site_name           VARCHAR(50),
+                system              VARCHAR(20),
+                ecosystem           VARCHAR(10),
+                location            TEXT,
+                st_x                DOUBLE PRECISION,
+                st_y                DOUBLE PRECISION,
+                abbreviation        VARCHAR(2),
+                waterbody_type_name VARCHAR(255),
+                waterbody_code      VARCHAR(100),
+                waterbody_name      VARCHAR(255),
+                created_date        timestamptz,
+                updated_date        timestamptz,
+                catchment           TEXT,
+                sample_count        BIGINT
+            )
+    language plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT s.site_id,
+               s.site_name,
+               sy.system_name           as system,
+               e.ecosystem_name         as ecosystem,
+               ST_AsGeoJSON(s.location) AS location,
+               st_x(s.location)         AS longitude,
+               st_y(s.location)         AS latitude,
+               st.abbreviation             us_state,
+               w.waterbody_type_name,
+               s.waterbody_code,
+               s.waterbody_name,
+               s.created_date,
+               s.updated_date,
+               ST_AsGeoJSON(s.catchment) AS catchment,
+               COUNT(ss.site_id)        AS sample_count
+        FROM geo.sites s
+                 LEFT JOIN geo.states st ON st_contains(st.geom, s.location)
+                 LEFT JOIN geo.systems sy ON s.system_id = sy.system_id
+                 LEFT JOIN geo.ecosystems e ON sy.ecosystem_id = e.ecosystem_id
+                 LEFT JOIN geo.waterbody_types w ON s.waterbody_type_id = w.waterbody_type_id
+                 LEFT JOIN sample.samples ss ON s.site_id = ss.site_id
+        WHERE s.site_id = p_site_id
+        GROUP BY s.site_id, s.site_name, sy.system_name, e.ecosystem_name, ST_AsGeoJSON(s.location), st_x(s.location),
+                 st_y(s.location), st.abbreviation, w.waterbody_type_name, s.waterbody_code, s.waterbody_name,
+                 s.created_date,
+                 s.updated_date, ST_AsGeoJSON(s.catchment);
+end
+$$;
+
+
+/******************************************************************************************************************
 taxa SCHEMA
 */
 DROP FUNCTION IF EXISTS taxa.fn_tree;
