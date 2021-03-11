b5e62fab6de379eab48461063a99b67ae6815615
Philip Bailey <philip@northarrowresearch.com>
2021-03-11 12:06:08 -0800

    models

--- docker/postgres/initdb/10_model_types.sql
+++ docker/postgres/initdb/10_model_types.sql
@@ -1,4 +1,4 @@
-INSERT INTO geo.model_types (model_type_id, abbreviation, model_type_name) VALUES (1, 'O2E', 'Observed Versus Expected');
+INSERT INTO geo.model_types (model_type_id, abbreviation, model_type_name) VALUES (1, 'OE', 'Observed Versus Expected');
 INSERT INTO geo.model_types (model_type_id, abbreviation, model_type_name) VALUES (2, 'MMI', 'Multimetric Index');
 INSERT INTO geo.model_types (model_type_id, abbreviation, model_type_name) VALUES (3, 'Hybrid', 'Both Observed Versus Expected and Multimetric Index');
 INSERT INTO geo.model_types (model_type_id, abbreviation, model_type_name) VALUES (4, 'Measurement', 'Measurement metric based on calculation');
