bd44ae8ac37b6e7e7a1afdb9065dbf0a07f443d8
Philip Bailey <philip@northarrowresearch.com>
2021-04-09 11:04:17 -0700

    unit testing

--- python/test/conftest.py
+++ python/test/conftest.py
@@ -1,17 +1,18 @@
+import os
 import pytest
 import psycopg2
+from psycopg2.extras import execute_values
 import textwrap
-from dotenv import load_dotenv
-import os
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
+
 
 @pytest.fixture(scope='module')
 def cnxn():
+
     cnxn = psycopg2.connect(
-        user=os.getenv('POSTGRES_USER'), 
-        password=os.getenv('POSTGRES_PASSWORD'), 
-        host=os.getenv('POSTGRES_HOST'), 
-        port=os.getenv('POSTGRES_PORT'), 
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host=os.getenv('POSTGRES_HOST'),
+        port=os.getenv('POSTGRES_PORT'),
         database=os.getenv('POSTGRES_DB'))
     yield cnxn
     cnxn.close()
@@ -24,41 +25,41 @@ def cursor(cnxn):
     cnxn.rollback()
 
 
-@pytest.fixture
-def birch_bookshelf_project(cursor):
-    stmt = textwrap.dedent('''
-    INSERT INTO projects(name, description)
-    VALUES ('bookshelf', 'Building a bookshelf from birch plywood')
-    RETURNING id
-    ''')
-    cursor.execute(stmt)
-    project_id = cursor.fetchone()[0]
-    stmt = textwrap.dedent('''
-    INSERT INTO project_supplies(project_id, name, quantity, unit_cost)
-    VALUES ({project_id}, 'Birch Plywood', 3, 48.50),
-           ({project_id}, 'Wood Glue', 1, 5.99),
-           ({project_id}, 'Screws', 2, 8.97),
-           ({project_id}, 'Stain', 1, 30.99)
-    ''')
-    cursor.execute(stmt.format(project_id=project_id))
-    yield project_id
+# @pytest.fixture
+# def birch_bookshelf_project(cursor):
+#     stmt = textwrap.dedent('''
+#     INSERT INTO projects(name, description)
+#     VALUES ('bookshelf', 'Building a bookshelf from birch plywood')
+#     RETURNING id
+#     ''')
+#     cursor.execute(stmt)
+#     project_id = cursor.fetchone()[0]
+#     stmt = textwrap.dedent('''
+#     INSERT INTO project_supplies(project_id, name, quantity, unit_cost)
+#     VALUES ({project_id}, 'Birch Plywood', 3, 48.50),
+#            ({project_id}, 'Wood Glue', 1, 5.99),
+#            ({project_id}, 'Screws', 2, 8.97),
+#            ({project_id}, 'Stain', 1, 30.99)
+#     ''')
+#     cursor.execute(stmt.format(project_id=project_id))
+#     yield project_id
 
 
-@pytest.fixture
-def raised_garden_bed_project(cursor):
-    stmt = textwrap.dedent('''
-    INSERT INTO projects(name, description)
-    VALUES ('bookshelf', 'Assemble a raised garden bed')
-    RETURNING id
-    ''')
-    cursor.execute(stmt)
-    project_id = cursor.fetchone()[0]
-    stmt = textwrap.dedent('''
-    INSERT INTO project_supplies(project_id, name, quantity, unit_cost)
-    VALUES ({project_id}, '4x4', 3, 3.75),
-           ({project_id}, '2x8', 8, 4.50),
-           ({project_id}, '2x4', 8, 2.50),
-           ({project_id}, 'Screws', 2, 8.97)
-    ''')
-    cursor.execute(stmt.format(project_id=project_id))
-    yield project_id
+# @pytest.fixture
+# def raised_garden_bed_project(cursor):
+#     stmt = textwrap.dedent('''
+#     INSERT INTO projects(name, description)
+#     VALUES ('bookshelf', 'Assemble a raised garden bed')
+#     RETURNING id
+#     ''')
+#     cursor.execute(stmt)
+#     project_id = cursor.fetchone()[0]
+#     stmt = textwrap.dedent('''
+#     INSERT INTO project_supplies(project_id, name, quantity, unit_cost)
+#     VALUES ({project_id}, '4x4', 3, 3.75),
+#            ({project_id}, '2x8', 8, 4.50),
+#            ({project_id}, '2x4', 8, 2.50),
+#            ({project_id}, 'Screws', 2, 8.97)
+#     ''')
+#     cursor.execute(stmt.format(project_id=project_id))
+#     yield project_id
--- python/test/test_database.py
+++ python/test/test_database.py
@@ -1,37 +1,43 @@
 from decimal import Decimal
-
-def test_project_table_exists(cursor, birch_bookshelf_project):
-    cursor.execute('select id from projects')
-    rs = cursor.fetchall()
-    assert len(rs) == 1
+import pytest
 
 
-def test_project_supplies_table_exists(cursor, birch_bookshelf_project):
-    cursor.execute('select id from project_supplies')
-    rs = cursor.fetchall()
-    assert len(rs) == 4
+def test_set_site_catchment_success(cursor):
+
+    # Valid polygon in Oregon
+    cursor.execute("""SELECT sample.fn_set_site_catchment(1, '{ "type": "Polygon", "coordinates": [ [ [ -122.68432617187497, 42.77927536024188 ], [ -116.53198242187499, 42.77927536024188 ], [ -116.53198242187499, 44.308126684886126 ], [ -122.68432617187497, 44.308126684886126 ], [ -122.68432617187497, 42.77927536024188 ] ] ] }')""")
+    assert cursor.fetchone()[0] == 1
+
+    # Valid polygon spanning Oregon coast and Pacific Ocean
+    cursor.execute("""SELECT sample.fn_set_site_catchment(1, '{ "type": "Polygon", "coordinates": [ [ [ -127.28759765624999, 41.21172151054787 ], [ -121.13525390625, 41.21172151054787 ], [ -121.13525390625, 42.779275360241904 ], [ -127.28759765624999, 42.779275360241904 ], [ -127.28759765624999, 41.21172151054787 ] ] ] }')""")
+    assert cursor.fetchone()[0] == 1
+
+    # Valid polygon spanning Canadian and US border
+    cursor.execute("""SELECT sample.fn_set_site_catchment(1, '{ "type": "Polygon", "coordinates": [ [ [ -118.37768554687497, 48.312427904071754 ], [ -112.22534179687499, 48.312427904071754 ], [ -112.22534179687499, 49.696061819115634 ], [ -118.37768554687497, 49.696061819115634 ], [ -118.37768554687497, 48.312427904071754 ] ] ] }')""")
+    assert cursor.fetchone()[0] == 1
 
 
-def test_project_steps_table_exists(cursor):
-    cursor.execute('select id from project_steps')
-    rs = cursor.fetchall()
-    assert len(rs) == 0
+def test_set_site_catchment_failure(cursor):
+
+    # Valid polygon but site ID that does not exist in database. Affected rows = 0
+    cursor.execute("""SELECT sample.fn_set_site_catchment(-1, '{ "type": "Polygon", "coordinates": [ [ [ -127.28759765624999, 41.21172151054787 ], [ -121.13525390625, 41.21172151054787 ], [ -121.13525390625, 42.779275360241904 ], [ -127.28759765624999, 42.779275360241904 ], [ -127.28759765624999, 41.21172151054787 ] ] ] }')""")
+    assert cursor.fetchone()[0] == 0
+
+    # Invalid polygon
+    with pytest.raises(Exception) as exinfo:
+        cursor.execute("""SELECT sample.fn_set_site_catchment(1, '{ "type": "Polygon", "coordinates": [ [[ -127.28759765624999, 41.21172151054787 ]]] }')""")
+    assert "invalid" in str(exinfo.value)
+
+    # Empty polygon
+    # with pytest.raises(Exception) as exinfo:
+    #     cursor.execute("""SELECT sample.fn_set_site_catchment(1, '{ "type": "Polygon", "coordinates": null }')""")
+    # assert "empty" in str(exinfo.value)
+
+    # Polygon off coast of Oregon in Pacific
+    with pytest.raises(Exception) as exinfo:
+        cursor.execute("""SELECT sample.fn_set_site_catchment(1, '{ "type": "Polygon", "coordinates": [ [ [ -131.91284179687497, 41.67291181960206 ], [ -125.76049804687499, 41.67291181960206 ], [ -125.76049804687499, 43.229195113965005 ], [ -131.91284179687497, 43.229195113965005 ], [ -131.91284179687497, 41.67291181960206 ] ] ] }')""")
+    assert "catchment does not intersect the United States" in str(exinfo.value)
 
 
-def test_get_project_costs(cursor, birch_bookshelf_project):
-    """This one is failing because I don't really know how to write procedures in postgres
-
-    Args:
-        cursor ([type]): [description]
-        birch_bookshelf_project ([type]): [description]
-    """
-    cursor.callproc('get_project_costs',[birch_bookshelf_project,])
-    cost = cursor.fetchval()
-    assert cost == Decimal('200.42')
-
-
-def test_projects_and_supplies(cursor, raised_garden_bed_project, birch_bookshelf_project):
-    stmt = 'SELECT * from projects_and_supplies'
-    cursor.execute(stmt)
-    rs = cursor.fetchall()
-    assert len(rs) == 8
+if __name__ == "__main__":
+    pytest.main()
