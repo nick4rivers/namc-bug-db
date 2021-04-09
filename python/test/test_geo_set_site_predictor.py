d7cd59eb8d1fb7d0ad4e9ab3d049020cc0c1e6f2
Philip Bailey <philip@northarrowresearch.com>
2021-04-09 13:24:32 -0700

    unit tests

---
+++ python/test/test_geo_set_site_predictor.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def test_set_site_predictor_success(cursor):
+
+    # integer predictor
+    cursor.execute("""SELECT sample.fn_set_site_predictor_value(2, 1, '{"value": 1}')""")
+    assert cursor.fetchone()[0] == 1
+
+    # float predictor
+    cursor.execute("""SELECT sample.fn_set_site_predictor_value(2, 1, '{"value": 0.5}')""")
+    assert cursor.fetchone()[0] == 1
+
+    # string predictor
+    cursor.execute("""SELECT sample.fn_set_site_predictor_value(2, 1, '{"value": "valid predictor value"}')""")
+    assert cursor.fetchone()[0] == 1
