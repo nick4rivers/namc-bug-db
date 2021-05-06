3a59945cddb3e4503ce1bb519f9bfc90980fdec6
Philip <philip@northarrowresearch.com>
2021-05-06 11:09:01 -0700

    metric schema
    SQL typo
    
    SQL typo
    
    SQL typo
    
    SQL typo
    
    SQL typo
    
    SQL typo
    
    SQL typo
    
    renaming metric types to groups
    
    fixing report type data
    
    fixing report analyses
    
    metric defs
    
    metric defs

--- docker/postgres/initdb/02_metric_functions.sql
+++ docker/postgres/initdb/02_metric_functions.sql
@@ -2,12 +2,10 @@ drop function if exists metric.fn_metrics;
 create or replace function metric.fn_metrics(p_limit int, p_offset int)
     returns table
             (
-                metric_id             int,
+                metric_id             smallint,
                 metric_name           varchar(255),
-                metric_type_id        smallint,
-                type_name             varchar(255),
-                translation_id        smallint,
-                translation_name      varchar(255),
+                group_id              smallint,
+                group_name            varchar(255),
                 formula_id            smallint,
                 formula_name          text,
                 formula_code_function varchar(100),
@@ -23,18 +21,16 @@ $$
 
 select m.metric_id,
        m.metric_name,
-       m.metric_type_id,
-       mt.type_name,
-       m.translation_id,
-       t.translation_name,
+       m.group_id,
+       mg.group_name,
        m.formula_id,
        f.display_text,
        f.code_function,
        m.is_standardized,
        m.perturb_direction,
        m.description,
-       to_json(m.created_date)#>> '{}',
-       to_json(m.updated_date)#>> '{}'
+       to_json(m.created_date) #>> '{}',
+       to_json(m.updated_date) #>> '{}'
 from metric.metrics m
          inner join
      (
@@ -43,7 +39,6 @@ from metric.metrics m
          order by mm.metric_id
          limit p_limit offset p_offset
      ) ml on m.metric_id = ml.metric_id
-         inner join metric.metric_types mt on m.metric_type_id = mt.metric_type_id
-         inner join taxa.translations t on m.translation_id = t.translation_id
+         inner join metric.metric_groups mg on m.group_id = mg.group_id
          left join metric.formulae f on m.formula_id = f.formula_id
 $$;
---
+++ docker/postgres/initdb/10_metric_groups.sql
@@ -0,0 +1,8 @@
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (1, 1, 'Collection Information', NULL);
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (2, 2, 'Laboratory Processing', NULL);
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (3, 3, 'Richness', '(metrics summarizing all unique taxa in a sample)');
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (4, 4, 'Dominance Metrics', '(metrics summarizing all most abundant taxa in a sample');
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (5, 5, 'Tolerance Indices', '(indices based on the indicator species concept in which taxa are assigned tolerance values)');
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (6, 6, 'Functional Feeding Groups', '(classification of organisms based on morphological or behavioral adaptations for where and how food is acquired)');
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (7, 7, 'Functional Traits', '(metrics based on morphological and life history traits)');
+INSERT INTO metric.metric_groups (group_id, sort_order, group_name, description) VALUES (8, 8, 'Compositional Metrics', '(richness and abundance of various taxonomic groups)');
