3a61f3a21f979fdc18289c3a6b695ba8f6d5565a
Philip Bailey <philip@northarrowresearch.com>
2021-05-25 11:49:36 -0700

    taxa SQLite database version 1

--- python/scripts/sqlite_taxa/schema.sql
+++ python/scripts/sqlite_taxa/schema.sql
@@ -18,27 +18,37 @@ CREATE TABLE taxonomy
     parent_id       int,
     author          VARCHAR(255),
     year            int,
+    status          text,
     created_date    TIMESTAMPTZ     NOT NULL DEFAULT current_timestamp,
     updated_date    TIMESTAMPTZ     NOT NULL DEFAULT current_timestamp,
 
     CONSTRAINT fk_taxonomy_taxa_level_id FOREIGN KEY (level_id) REFERENCES taxa_levels (level_id)
 );
-CREATE index fx_taxonomy_level_id on taxonomy(level_id);
-create index fx_taxonomy_parent_id on taxonomy(parent_id);
+CREATE index fx_taxonomy_level_id on taxonomy (level_id);
+create index fx_taxonomy_parent_id on taxonomy (parent_id);
 
 CREATE TRIGGER tr_taxonomy
-    before update
+    AFTER UPDATE OF scientific_name,
+        level_id,
+        parent_id,
+        author,
+        year,
+        status
     ON taxonomy
-    for each row
+    FOR EACH ROW
 BEGIN
-    update taxonomy set updated_date = current_timestamp where taxonomy_id = new.taxonomy_id;
+    UPDATE taxonomy
+    SET updated_date = CURRENT_TIMESTAMP
+    WHERE taxonomy_id = new.taxonomy_id;
 END;
+
 
 CREATE TABLE synonyms
 (
     synonym_id  INT          NOT NULL PRIMARY KEY,
     taxonomy_id int          NOT NULL,
     synonym     VARCHAR(255) NOT NULL,
+    status      text,
 
     CONSTRAINT fk_synonyms_taxonomy_id FOREIGN KEY (taxonomy_id) REFERENCES taxonomy (taxonomy_id) ON DELETE CASCADE
 );
@@ -95,6 +105,7 @@ CREATE TABLE external_ids
     source_id          int NOT NULL,
     taxonomy_id        int NOT NULL,
     external_source_id VARCHAR(255),
+    status             text,
 
     CONSTRAINT pk_external_ids PRIMARY KEY (source_id, taxonomy_id),
     CONSTRAINT fk_external_ids_source_id FOREIGN KEY (source_id) REFERENCES external_sources (source_id) ON DELETE CASCADE,
@@ -104,28 +115,38 @@ CREATE TABLE external_ids
 
 CREATE TABLE translations
 (
-    translation_id   int not null PRIMARY KEY,
+    translation_id   int                 not null PRIMARY KEY,
     translation_name VARCHAR(255) UNIQUE NOT NULL,
     description      TEXT,
     is_active        BOOLEAN             NOT NULL DEFAULT TRUE,
+    status           text,
 
     created_date     TIMESTAMPTZ         NOT NULL DEFAULT current_timestamp,
     updated_date     TIMESTAMPTZ         NOT NULL DEFAULT current_timestamp
 );
+
 CREATE TRIGGER tr_translations
-    before update
-    ON translations
+         AFTER UPDATE OF translation_name,
+                         description,
+                         is_active,
+                         status
+            ON translations
+      FOR EACH ROW
 BEGIN
-    update translations set updated_date = current_timestamp where translation_id = new.translation_id;
+    UPDATE translations
+       SET updated_date = CURRENT_TIMESTAMP
+     WHERE translation_id = new.translation_id;
 END;
+
 
 
 CREATE TABLE translation_taxa
 (
-    translation_taxonomy_id   INT not null PRIMARY KEY,
+    translation_taxonomy_id   INTEGER PRIMARY KEY AUTOINCREMENT,
     translation_id            int         NOT NULL,
     taxonomy_id               int         NOT NULL,
     translation_taxonomy_name VARCHAR(255),
+    status                    text,
     created_date              TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
     updated_date              TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
 
@@ -136,8 +157,40 @@ CREATE INDEX fx_translation_taxa_taxonomy_id ON translation_taxa (taxonomy_id);
 CREATE UNIQUE INDEX ux_translation_taxa_taxonomy_id ON translation_taxa (translation_id, taxonomy_id);
 
 CREATE TRIGGER tr_translation_taxa
-    before update
-    ON translation_taxa
+         AFTER UPDATE OF translation_id,
+                         taxonomy_id,
+                         translation_taxonomy_name,
+                         status
+            ON translation_taxa
+      FOR EACH ROW
 BEGIN
-    update translation_taxa set updated_date = current_timestamp where translation_taxonomy_id = new .translation_taxonomy_id;
+    UPDATE translation_taxa
+       SET updated_date = CURRENT_TIMESTAMP
+     WHERE translation_taxonomy_id = new.translation_taxonomy_id;
 END;
+
+
+
+
+CREATE VIEW vwTaxonomy AS
+SELECT t.taxonomy_id,
+       tl.level_name,
+       t.scientific_name,
+       t.year,
+       t.author,
+       t.status,
+       p.parent_id,
+       pl.level_name
+FROM taxonomy t
+         inner join taxa_levels tl on t.level_id = tl.level_id
+         inner join taxonomy p on t.parent_id = p.taxonomy_id
+         inner join taxa_levels pl on p.level_id = pl.level_id
+order by tl.rank_order;
+
+CREATE VIEW vwTranslationTaxa AS
+SELECT tt.translation_id, t.translation_name, tt.taxonomy_id, t2.scientific_name, tl.level_name, tt.translation_taxonomy_name
+FROM translation_taxa tt
+         inner join translations t on tt.translation_id = t.translation_id
+inner join taxonomy t2 on tt.taxonomy_id = t2.taxonomy_id
+inner join taxa_levels tl on t2.level_id = tl.level_id
+order by t.translation_name, tl.rank_order;
