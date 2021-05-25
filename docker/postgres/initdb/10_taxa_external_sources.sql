0c1b49db2550ef3576102ac3e02ac907ba9ff393
Philip Bailey <philip@northarrowresearch.com>
2021-05-25 11:50:22 -0700

    external sources lookup data

---
+++ docker/postgres/initdb/10_taxa_external_sources.sql
@@ -0,0 +1,2 @@
+INSERT INTO taxa.external_sources (source_id, source_name, abbreviation) VALUES (1, 'Integrated Taxonomic Information System', 'ITIS');
+INSERT INTO taxa.external_sources (source_id, source_name, abbreviation) VALUES (2, 'Encyclopedia of Life', 'EoL');
