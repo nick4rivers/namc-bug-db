e45ead192a9442d78fa393cf1dd6f813fbb07a94
Philip Bailey <philip@northarrowresearch.com>
2021-06-14 15:14:13 -0700

    30_metric_attributes.sql

---
+++ docker/postgres/initdb/30_metric_attributes.sql
@@ -0,0 +1,44 @@
+-- Hilsenhoff Biotic Index
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '1');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '2');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '3');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '4');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '5');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '6');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '7');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '8');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '9');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (31, 6, '10');
+-- # of Intolerant Taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (32, 6, '1');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (32, 6, '2');
+-- Abundance of Intolerant Taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (33, 6, '1');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (33, 6, '2');
+-- # Tolerant Taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (34, 6, '8');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (34, 6, '9');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (34, 6, '10');
+-- Tolerance Taxa Abundance
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (35, 6, '8');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (35, 6, '9');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (35, 6, '10');
+-- Richness and Abundance of shredder taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (37, 3, 'SH');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (38, 3, 'SH');
+-- Richness and Abundance of scraper taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (39, 3, 'SC');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (40, 3, 'SC');
+-- Richness and Abundance of collector filterer taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (41, 3, 'CF');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (42, 3, 'CF');
+-- Richness and Abundance of collector gatherer taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (43, 3, 'CG');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (44, 3, 'CG');
+-- Richness and Abundance of predator taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (45, 3, 'PR');
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (46, 3, 'PR');
+-- Richness of Clinger Taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (47, 9, 'Y');
+-- Richness of Long Lived Taxa
+INSERT INTO metric.metric_attributes (metric_id, attribute_id, where_clause) VALUES (48, 8, 'Y');
