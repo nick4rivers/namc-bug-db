da0827dc844e5eebe3ebef07bece8f43b9bdde40
Philip Bailey <philip@northarrowresearch.com>
2021-07-05 14:49:37 -0700

    fixing abundance to always return 0 not NULL

--- python/test/test_abundance.py
+++ python/test/test_abundance.py
@@ -64,22 +64,39 @@ def test_sample_abundance(cursor, taxonomic_hierarchy, abundance):
     assert sa[0] == 20
 
 
-# def test_taxa_abundance(cursor, taxonomic_hierarchy, abundance):
+def test_richness_by_taxa(cursor):
+    """ Need to test that when calculating the richness for a specific
+    metric that filters by taxa, the result is zero if there are no
+    occurrences of that taxa in the sample
+    """
 
-#     samples = get_samples_by_customer_name(cursor, 'test customer')
-#     sample_id = samples[0]
+    cursor.execute("INSERT INTO taxa.taxonomy (scientific_name, level_id) VALUES ('test taxa', 15) returning taxonomy_id")
+    taxonomy_id = cursor.fetchone()[0]
 
-#     # Abundance at the Phylum should be all organisms for the sample
-#     phyluma = get_taxa_by_name(cursor, 'Test Phylum A')
-#     cursor.execute('SELECT * FROM metric.fn_taxa_abundance(%s, %s)', [sample_id, phyluma])
-#     phyluma_abundance = cursor.fetchone()[0]
-#     assert phyluma_abundance == 20
+    cursor.execute("INSERT INTO metric.metrics (metric_name, group_id) VALUES ('Test Metric', 1) returning metric_id")
+    metric_id = cursor.fetchone()[0]
 
-#     # Abundance at the class under the phylum should just be the class
-#     classaa = get_taxa_by_name(cursor, 'Test Class AA')
-#     cursor.execute('SELECT metric.fn_taxa_abundance(%s, %s)', [sample_id, classaa])
-#     classaa_abundance = cursor.fetchone()[0]
-#     assert classaa_abundance == 10
+    cursor.execute('INSERT INTO metric.metric_taxa(metric_id, taxonomy_id) VALUES (%s, %s)', [metric_id, taxonomy_id])
+
+    # test a sample that does include the metric taxa
+    cursor.execute("""select * from metric.fn_taxa_abundance(%s, Array[
+        (1,'taxa 1',1,'level a',100)::taxa_info2,
+        (2,'taxa 2',1,'level b',200)::taxa_info2,
+        (3,'taxa 3',1,'level c',300)::taxa_info2,
+        (1,'taxa 1',1,'level a',100)::taxa_info2,
+        ({0},'taxa {0}',1,'level e',1)::taxa_info2
+        ], Array[]::taxa_info2[])""".format(taxonomy_id), [metric_id])
+    assert cursor.fetchone()[0] == 1
+
+    # Now test a sample that does NOT include the metric taxa
+    cursor.execute("""select * from metric.fn_taxa_abundance(%s, Array[
+        (1,'taxa 1',1,'level a',100)::taxa_info2,
+        (2,'taxa 2',1,'level b',200)::taxa_info2,
+        (3,'taxa 3',1,'level c',300)::taxa_info2,
+        (1,'taxa 1',1,'level a',100)::taxa_info2,
+        (5,'taxa 5',1,'level e',1)::taxa_info2
+        ], Array[]::taxa_info2[])""", [metric_id])
+    assert cursor.fetchone()[0] == 0
 
 
 # def test_attribute_abundance(cursor, taxonomic_hierarchy, taxonomic_attributes, abundance):
